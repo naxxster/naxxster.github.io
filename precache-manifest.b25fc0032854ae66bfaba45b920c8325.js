aee5cfe72c3af3da73e44cd104224c6c48b09028
Junsong Jang <junsjang@amazon.com>
2019-09-15 21:51:46 +0900

    Initial commit

---
+++ precache-manifest.b25fc0032854ae66bfaba45b920c8325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6f54385467b1de4a8ec3b740b88c92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5519bf662087b79f98a9",
+    "url": "/static/css/2.eee0f4a3.chunk.css"
+  },
+  {
+    "revision": "cacef0d683b77bf2e78e",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "5519bf662087b79f98a9",
+    "url": "/static/js/2.eec4d0f8.chunk.js"
+  },
+  {
+    "revision": "cacef0d683b77bf2e78e",
+    "url": "/static/js/main.e0cf4b26.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  }
+]);
